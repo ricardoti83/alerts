97aac2f22ef0843ac57310706f62a1b9df07f381
jo316it <ricardofromit@gmail.com>
2024-10-11 13:37:54 -0300

    ok

--- main.py
+++ main.py
@@ -4,54 +4,10 @@ from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.subscription import SubscriptionClient
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
+from datetime import datetime
 
 # Carregar as variáveis de ambiente do arquivo .env
 load_dotenv()
-
-EMAIL_USER = os.getenv('EMAIL_USER')
-EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
-EMAIL_RECIPIENT = os.getenv('EMAIL_RECIPIENT')
-
-def send_email_with_csv(csv_file_path):
-    fromaddr = "contato@cloudwings.com.br"
-    toaddr = "ricardofromit@gmail.com"
-    msg = MIMEMultipart()
-
-    # Configurações do e-mail
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = "Relatório de Recursos Sem Tags"
-
-    # Corpo do e-mail
-    body = "Segue em anexo o relatório de recursos sem tags na assinatura."
-
-    msg.attach(MIMEText(body, 'plain'))
-
-    # Anexar o arquivo CSV
-    filename = os.path.basename(csv_file_path)
-    attachment = open(csv_file_path, "rb")
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', f"attachment; filename= {filename}")
-    msg.attach(part)
-
-    # Configurações do servidor SMTP (por exemplo, Gmail)
-    server = smtplib.SMTP('smtp.umbler.com', 587)
-    server.starttls()
-    server.login(fromaddr, "Fl0ki1983@!")
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
-    server.quit()
-
-# Exemplo de uso
-csv_file_path = '/app/data/resources_without_tags.csv'
-send_email_with_csv(csv_file_path)
 
 # Função para obter todas as assinaturas associadas ao Service Principal
 def get_all_subscriptions():
@@ -79,6 +35,9 @@ def save_results_to_csv(results, output_file):
     """Salva os resultados em um arquivo CSV."""
     keys = results[0].keys() if results else []
     
+    # Criar diretório se não existir
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
     with open(output_file, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=keys)
         writer.writeheader()
@@ -98,22 +57,21 @@ def alert_resources_without_tags():
         resources_without_tags = list_resources_without_tags(subscription_id, credential)
         
         for resource in resources_without_tags:
+            creation_time = resource.properties.get("creationTime") if resource.properties and "creationTime" in resource.properties else "Data não disponível"
             result = {
                 "subscription_id": subscription_id,
                 "resource_name": resource.name,
                 "resource_type": resource.type,
-                "resource_location": resource.location
+                "resource_location": resource.location,
+                "creation_time": creation_time,  # Adicionando a data e hora de criação
+                "checked_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Data e hora atual da verificação
             }
             all_results.append(result)
     
     # Definir o caminho para salvar o arquivo CSV fora do container
-    output_path = '/data/resources_without_tags.csv'
+    output_path = '/app/data/resources_without_tags.csv'
     save_results_to_csv(all_results, output_path)
     print(f"Resultados salvos em {output_path}")
 
-    send_email_with_csv(csv_file_path)
-    print(f"Enviando o email")
-
 if __name__ == "__main__":
     alert_resources_without_tags()
-    
